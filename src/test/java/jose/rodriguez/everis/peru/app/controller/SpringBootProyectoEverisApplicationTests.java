30fbdf72f87e675abdd098bd52cb3438954181c7
JoseR <rodri@gmail.com>
2019-09-19 14:09:12 -0500

    Comentarios

--- src/test/java/jose/rodriguez/everis/peru/app/controller/SpringBootProyectoEverisApplicationTests.java
+++ src/test/java/jose/rodriguez/everis/peru/app/controller/SpringBootProyectoEverisApplicationTests.java
@@ -138,7 +138,7 @@ public class SpringBootProyectoEverisApplicationTests {
   
   @Test
   public void eliminarTest() {
-    Student student = service.findByName("Soyla").block();
+    Student student = service.findByName("Katty").block();
     client.delete()
         .uri("/api/everis/students/{id}", Collections.singletonMap("id", student.getId()))
         .exchange().expectStatus().isNoContent().expectBody().isEmpty();
